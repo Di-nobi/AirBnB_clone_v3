5e0fd492c6905381f1d8f8f0193ad3198fd458ae
Dinobi <architectstan01@gmail.com>
2023-05-29 06:43:13 -0700

    Flask Task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 """ Creates a new view for state """
-from flask import jsonify, make_response
+from flask import jsonify, make_response, abort
 from models.state import State
 from models import storage
 
+def state():
+    new_state = []
+    for state in storage.all(State).values():
+        new_state.append(state)
+    return jsonify(new_state.to_dict()), 200
 @app_views.route('/api/v1/states/<state_id>', method=['GET'])
 def state_retrieval(state_id):
     """ Retrieve state """
-    find_state = storage(State, state_id)
+    find_state = storage.get(State, state_id)
     if find_state is None:
         abort(404)
     else:
         return jsonify(find_state.to_dict())
 
-@app_views.route('/api/v1/states/
+@app_views.route('/api/v1/states/<state_id>' method=['DELETE'])
+def delete():
+    """ deletes a state """
+    del_state = storage.get(State, state_id)
+    storage.delete(del_state)
+    storage.save()
+    if not del_state:
+        abort(404)
+    return jsonify({}), 200
+
