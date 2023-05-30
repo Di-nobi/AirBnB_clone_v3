916ecc23a100d79fc9f52caa93ef7da9544b2064
Dinobi <architectstan01@gmail.com>
2023-05-30 06:58:02 -0700

    Flask Restful API

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Starts a flask application """
+from flask import Flask, jsonify
+from models import storage
+from app.v1.views import app_views
+import os
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(e):
+    """ Closes Storage """
+    storage.close()
+
+@app.errorhandler(404)
+def error(e):
+    """ Handles error """
+    return jsonify({'error': 'Not found'}), 404
+
+if __name__ == "__main__":
+    """ Function """
+    host = os.environ('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+""" Starting a Flask Application """
+from api.v1.views import app_views
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"}), 200
