6a72f3f5a1f7736e625a3d71318e5ea61dc4a18b
Dinobi <architectstan01@gmail.com>
2023-05-30 07:24:08 -0700

    Restful api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """ Starting a Flask Application """
 from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status():
+    """ Returns Status as OK """
     return jsonify({"status": "OK"}), 200
