d46d958926eafd5f3e0cf9864c5ba0311efa4932
Dinobi <architectstan01@gmail.com>
2023-05-30 07:18:24 -0700

    Restful Api

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Starts a flask application """
 from flask import Flask, jsonify
 from models import storage
-from app.v1.views import app_views
+from api.v1.views import app_views
 import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +19,6 @@ def error(e):
 
 if __name__ == "__main__":
     """ Function """
-    host = os.environ('HBNB_API_HOST', '0.0.0.0')
-    port = os.environ('HBNB_API_PORT', '5000')
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
