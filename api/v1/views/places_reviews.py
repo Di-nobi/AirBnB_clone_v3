7ec24c613ae3590f590afa939d8d141eb1a74065
Dinobi <architectstan01@gmail.com>
2023-06-01 08:10:21 -0700

    Airbnb task

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -17,14 +17,14 @@ def get_review(place_id):
     for review in place.reviews:
         get_review.append(review.to_dict())
     return jsonify(get_review)
-@app_views.route('/reviews/review_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def a_review(review_id):
     """ Retrieves a single review of a place """
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
     return jsonify(review.to_dict), 200
-@app_views.route('/reviews/review_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
 def del_review(review_id):
     """ Deletes a review of a place """
     review = storage.get(Review, review_id)
@@ -54,7 +54,7 @@ def post_review(place_id):
     ins.save()
     return jsonify(ins.to_dict()), 201
 
-@app_views.route('/reviews/review_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def put_review(review_id):
     """ Puts in Review """
     review = storage.get(Review, review_id)
